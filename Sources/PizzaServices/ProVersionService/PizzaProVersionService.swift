a448beacbf9894a23ade4ace6321fd91aeaf2278
Olesya Tranina <o.tranina@vk.team>
2023-07-30 20:31:39 +0100

    modify pro version service

--- Sources/PizzaServices/ProVersionService/PizzaProVersionService.swift
+++ Sources/PizzaServices/ProVersionService/PizzaProVersionService.swift
@@ -3,38 +3,55 @@ import Foundation
 import Combine
 import UIKit
 
+public struct ProVersionState {
+    let value: Bool
+    let forceValue: Bool
+
+    public var isPro: Bool {
+        forceValue || value
+    }
+}
+
 public protocol PizzaProVersionService {
     var value: Bool { get }
     var valuePublisher: AnyPublisher<Bool, Never> { get }
+
+    func setValue(_ value: Bool)
+    func setForceValue(_ forceValue: Bool)
 }
 
-public protocol PizzaProVersionWritableService: PizzaProVersionService {
-    var value: Bool { get set }
-}
-
-public class PizzaProVersionServiceImpl: PizzaProVersionWritableService {
+public class PizzaProVersionServiceImpl: PizzaProVersionService {
 
     // MARK: - Properties
 
     public var valuePublisher: AnyPublisher<Bool, Never> {
-        currentValueSubject.eraseToAnyPublisher()
+        currentValueSubject
+            .map { $0.isPro }
+            .eraseToAnyPublisher()
     }
     public var value: Bool {
-        get {
-            currentValueSubject.value
-        }
-        set {
-            currentValueSubject.send(newValue)
-        }
+        currentValueSubject.value.isPro
     }
 
-    private let currentValueSubject: CurrentValueSubject<Bool, Never>
+    private let currentValueSubject: CurrentValueSubject<ProVersionState, Never>
     private var bag = Set<AnyCancellable>()
 
     // MARK: - Initalization
 
     public init() {
-        currentValueSubject = .init(false)
+        currentValueSubject = .init(.init(value: false, forceValue: false))
+    }
+
+    // MARK: - Public Methods
+
+    public func setValue(_ value: Bool) {
+        let currentState = currentValueSubject.value
+        currentValueSubject.send(.init(value: value, forceValue: currentState.forceValue))
+    }
+
+    public func setForceValue(_ forceValue: Bool) {
+        let currentState = currentValueSubject.value
+        currentValueSubject.send(.init(value: currentState.value, forceValue: forceValue))
     }
 
 }
