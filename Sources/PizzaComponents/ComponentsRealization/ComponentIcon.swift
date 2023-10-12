9d309a9ae288fa8e0657affded7581eeeedc7fb9
Alexander Filimonov <a.filimonov@prequelapp.com>
2023-10-12 11:27:59 +0100

    add hierarchical rendering icon

--- Sources/PizzaComponents/ComponentsRealization/ComponentIcon.swift
+++ Sources/PizzaComponents/ComponentsRealization/ComponentIcon.swift
@@ -5,18 +5,27 @@ import PizzaDesign
 public enum ComponentIcon {
 
     public struct SystemSquareRounded {
+
+        public enum RenderingMode {
+            case hierarchical
+            case oneColor
+        }
+
         public let sfSymbol: SFSymbol
         public let tintColor: UIColor
         public let backgroundColor: UIColor?
+        public let renderingMode: RenderingMode
 
         public init(
             sfSymbol: SFSymbol,
             tintColor: UIColor = .white,
-            backgroundColor: UIColor? = nil
+            backgroundColor: UIColor? = nil,
+            renderingMode: RenderingMode = .oneColor
         ) {
             self.sfSymbol = sfSymbol
             self.tintColor = tintColor
             self.backgroundColor = backgroundColor
+            self.renderingMode = renderingMode
         }
     }
 
@@ -54,14 +63,31 @@ public class ComponentSystemIconView: PizzaView {
     public func configure(
         sfSymbol: SFSymbol?,
         tintColor: UIColor?,
-        backgroundColor: UIColor?
+        backgroundColor: UIColor?,
+        renderingMode: ComponentIcon.SystemSquareRounded.RenderingMode?
     ) {
         if let sfSymbol {
             foregroundImageView.image = UIImage(
                 systemName: sfSymbol.rawValue,
-                withConfiguration: UIImage.SymbolConfiguration(
-                    pointSize: backgroundColor == nil ? 22 : 16
-                )
+                withConfiguration: {
+                    var conf = UIImage.SymbolConfiguration(
+                        pointSize: backgroundColor == nil ? 22 : 16
+                    )
+
+                    if 
+                        let renderingMode,
+                        renderingMode == .hierarchical,
+                        let tintColor
+                    {
+                        conf = conf.applying(
+                            UIImage.SymbolConfiguration(
+                                hierarchicalColor: tintColor
+                            )
+                        )
+                    }
+
+                    return conf
+                }()
             )
             foregroundImageView.tintColor = tintColor
         }
@@ -112,7 +138,8 @@ public class ComponentIconView: PizzaView {
             systemIconView.configure(
                 sfSymbol: payload.sfSymbol,
                 tintColor: payload.tintColor,
-                backgroundColor: payload.backgroundColor
+                backgroundColor: payload.backgroundColor,
+                renderingMode: payload.renderingMode
             )
             systemIconView.isHidden = false
             imageView.isHidden = true
@@ -120,7 +147,8 @@ public class ComponentIconView: PizzaView {
             systemIconView.configure(
                 sfSymbol: nil,
                 tintColor: nil,
-                backgroundColor: backgroundColor
+                backgroundColor: backgroundColor,
+                renderingMode: nil
             )
             systemIconView.isHidden = false
             imageView.isHidden = true
